d31391660b99232c2ca878ac090712d9b6072f0f
wuey99 <wuey99@gmail.com>
2020-10-08 11:21:03 +0100

    work-in-progress Terrain Editor

---
+++ src/scripts/editor/TerrainTileIcon.ts
@@ -0,0 +1,45 @@
+//------------------------------------------------------------------------------------------
+import * as PIXI from 'pixi.js'
+import { XApp } from '../app/XApp';
+import { XSprite } from '../sprite/XSprite';
+import { XSpriteLayer } from '../sprite/XSpriteLayer';
+import { XSignal } from '../signals/XSignal';
+import { XSignalManager } from '../signals/XSignalManager';
+import { world } from '../app';
+import { XTask } from '../task/XTask';
+import { XTaskManager} from '../task/XTaskManager';
+import { XTaskSubManager} from '../task/XTaskSubManager';
+import { XWorld} from '../sprite/XWorld';
+import { XDepthSprite} from '../sprite/XDepthSprite';
+import { XType } from '../type/Xtype';
+import { XGameObject} from '../gameobject/XGameObject';
+
+//------------------------------------------------------------------------------------------
+export class TerrainTileIcon extends XGameObject {
+	
+//------------------------------------------------------------------------------------------	
+	constructor () {
+		super ();
+	}
+	
+//------------------------------------------------------------------------------------------
+	public setup (__world:XWorld, __layer:number, __depth:number):XGameObject {
+        super.setup (__world, __layer, __depth);
+
+		return this;
+	}
+	
+//------------------------------------------------------------------------------------------
+	public afterSetup (__params:Array<any> = null):XGameObject {
+        super.afterSetup (__params);
+
+		return this;
+	}
+	
+//------------------------------------------------------------------------------------------
+	public cleanup():void {
+        super.cleanup ();
+	}
+	
+//------------------------------------------------------------------------------------------
+}
